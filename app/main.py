6f03ec537f3ec9df5155a0c2f11c2cef34be7f12
SiddharthaSarkar1 <mail.04.siddhartha@gmail.com>
2026-03-21 04:35:25 +0530

    field_validator and model_validator

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, HTTPException, Query
+from fastapi import FastAPI, HTTPException, Query, Path
 from service.product import get_all_products
+from schema.product import Product
 
 app = FastAPI()
 
@@ -20,7 +21,7 @@ def get_all_the_products():
     return get_all_products()
 
 
-# /products?name="XYZ"
+# /products?name="XYZ"....
 @app.get("/products")
 def list_products(
     name: str = Query(
@@ -28,9 +29,23 @@ def list_products(
         min_length=1,
         max_length=50,
         description="Search product by name (case insensitive)",
+        examples="furniture",
     ),
     sort_by_price: bool = Query(default=False, description="Sort by product price"),
-    order: str = Query(default="asc", description="Sort order when sort_by_price=true (asc, desc)")
+    order: str = Query(
+        default="asc", description="Sort order when sort_by_price=true (asc, desc)"
+    ),
+    limit: int = Query(
+        default=5,
+        ge=1,
+        le=50,
+        description="No of items to return",
+    ),
+    offset: int = Query(
+        default=0,
+        ge=0,
+        description="Pagination offset",
+    ),
 ):
     data = get_all_products()
     products = data["products"]
@@ -46,11 +61,40 @@ def list_products(
             raise HTTPException(
                 status_code=404, detail=f"No product found with matching name={name}"
             )
-        
+
         if sort_by_price:
             reverse = order == "desc"
-            matching_products = sorted(matching_products, key=lambda p: p.get("price", 0), reverse=reverse)
+            matching_products = sorted(
+                matching_products, key=lambda p: p.get("price", 0), reverse=reverse
+            )
 
+    matching_products = matching_products[offset : offset + limit]
     total = len(matching_products)
-
     return {"total": total, "products": matching_products}
+
+
+@app.get("/products/{product_id}")
+def get_prodict_by_id(
+    product_id: int = Path(
+        ..., ge=1, le=101, description="Id of the product", examples="1"
+    )
+):
+    data = get_all_products()
+    products = data["products"]
+
+    for product in products:
+        if product["id"] == product_id:
+            return product
+
+    raise HTTPException(
+        status_code=404, detail=f"Product not found with id={product_id}"
+    )
+
+
+# POST Routes
+
+
+@app.post("/products")
+def add_products(product: Product):
+    print(product)
+    return product
---
+++ app/schema/product.py
@@ -0,0 +1,59 @@
+from pydantic import BaseModel, Field, field_validator, model_validator, computed_field
+from typing import Annotated, List, Optional
+from datetime import datetime
+
+class Product(BaseModel):
+    id: int
+    title: str
+    description: str
+    category: str
+    price: float
+    discountPercentage: float
+    rating: float
+    stock: int
+    is_active: bool
+    tags: Annotated[
+        Optional[List[str]],
+        Field(default=None, max_length=10, description="Upto 10 tags"),
+    ]
+    brand: str
+    sku: Annotated[
+        str,
+        Field(
+            min_length=6,
+            max_length=30,
+            title="SKU",
+            description="Stock keeping unit",
+            examples=["grt-345-jdr-342", "rta-435-j78-lkf"],
+        ),
+    ]
+    weight: int
+    createdAt: datetime
+
+
+
+    @field_validator("sku", mode="after")
+    @classmethod
+
+    def validate_sku_format(cls, value: str):
+        if "-" not in value:
+            raise ValueError("SKU must have '-'")
+        
+        last = value.split("-")[-1]
+        if not (len(last) == 3 and last.isdigit()):
+            raise ValueError("SKU end with a 3-digit sequence like -234")
+        
+        return value
+
+    @model_validator(mode="after")
+    @classmethod
+    def validate_business_rule(cls, model: "Product"):
+        if model.stock == 0 and model.is_active == True:
+            raise ValueError("If stock is 0, is_active must be false")
+        
+        if model.discountPercentage > 0 and model.rating == 0:
+            raise ValueError("Discounted product mush have the rating (rating != 0)")
+        
+        return model
+
+
